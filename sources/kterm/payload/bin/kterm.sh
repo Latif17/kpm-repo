f7b772ec579e91ca93e20fb4825805856595f85c
Latif Muhammad <latif17@icloud.com>
2026-08-12 13:22:52 +0100

    fix: kterm colour scheme

--- sources/kterm/payload/bin/kterm.sh
+++ sources/kterm/payload/bin/kterm.sh
@@ -2,10 +2,11 @@
 EXTENSION="$(cd "$(dirname "$0")/.." && pwd)"
 DPI=`cat /var/log/Xorg.0.log | grep DPI | sed -n 's/.*(\([0-9]\+\), [0-9]\+).*/\1/p'`
 #use different layouts for high resolution devices
+PARAM="-c 0"
 if [ -n "${DPI}" ] && [ "${DPI}" -gt 290 ]; then
-  PARAM="-l ${EXTENSION}/layouts/keyboard-300dpi.xml"
+  PARAM="${PARAM} -l ${EXTENSION}/layouts/keyboard-300dpi.xml"
 elif [ -n "${DPI}" ] && [ "${DPI}" -gt 200 ]; then
-  PARAM="-l ${EXTENSION}/layouts/keyboard-200dpi.xml"
+  PARAM="${PARAM} -l ${EXTENSION}/layouts/keyboard-200dpi.xml"
 fi
 export TERM=xterm TERMINFO=${EXTENSION}/vte/terminfo
 ${EXTENSION}/bin/kterm ${PARAM} "$@"
