a32228f4228af8823db19392ca033e7454f71f32
Latif Muhammad <latif17@icloud.com>
2026-08-12 15:23:01 +0100

    feat: remove broken packages

--- sources/kindlefetch/payload/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-"${SCRIPT_DIR}/../../kterm/bin/kterm" -e "bash ${SCRIPT_DIR}/bin/kindlefetch.sh" -k 1 -o U -s 7
--- sources/kindlefetch/uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Uninstalling KindleFetch..."
-# Since it runs in-place, uninstalling is handled by the package manager deleting the package directory.
-echo "KindleFetch uninstalled successfully."
